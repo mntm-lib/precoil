803f8e0e915696e29b6138f895ecff9ea418fe38
eolme <im@petrov.engineer>
2021-12-09 23:19:50 +0400

    Add JSDoc

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,16 +1,32 @@
 import type { Atom, Selector } from './types.js';
 
-import { useLayoutEffect, useState } from 'react';
-import { constDeps, useCreation, useHandler } from '@mntm/shared';
+import { useState } from 'react';
+import { constDeps, useCreation, useHandler, useIsomorphicEffect } from '@mntm/shared';
 
 import { updater } from './store.js';
 
-/** @noinline */
+/**
+ * @description This is the recommended hook to use when a component intends to read computed state.
+ * Using this hook in a React component will subscribe the component to re-render when the computed
+ * state is updated.
+ *
+ * @returns The computed value of the given atom state.
+ *
+ * @example
+ *
+ *   // Somewhere outside the component, for example, next to the declaration of the atom:
+ *   const selectPlus = (value) => value + 1;
+ *
+ *   // In the component:
+ *   const plus = useAtomSelector(counterAtom, selectPlus);
+ *
+ * @noinline
+ */
 export const useAtomSelector = /*#__NOINLINE__*/<T, S>(atom: Readonly<Atom<T>>, selector: Selector<T, S>) => {
   const [state, handleState] = useState(() => selector(atom.get()));
 
   // Use layout effect for preventing race condition
-  useLayoutEffect(() => {
+  useIsomorphicEffect(() => {
     const handleSelect = (value: T) => handleState(selector(value));
 
     updater.on(atom.key, handleSelect);
@@ -21,12 +37,24 @@ export const useAtomSelector = /*#__NOINLINE__*/<T, S>(atom: Readonly<Atom<T>>, 
   return state;
 };
 
-/** @nosideeffects */
+/**
+ * @description This is the recommended hook to use when a component intends to read state
+ * without writing to it. Using this hook in a React component will subscribe the component
+ * to re-render when the state is updated.
+ *
+ * @returns The value of the given atom state.
+ *
+ * @example
+ *
+ *   const counter = useAtomValue(counterAtom);
+ *
+ * @noinline
+ */
 export const useAtomValue = /*#__NOINLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   const [state, handleState] = useState(atom.get);
 
   // Use layout effect for preventing race condition
-  useLayoutEffect(() => {
+  useIsomorphicEffect(() => {
     updater.on(atom.key, handleState);
 
     return () => updater.off(atom.key, handleState);
@@ -35,12 +63,37 @@ export const useAtomValue = /*#__NOINLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   return state;
 };
 
-/** @nosideeffects */
+/**
+ * @description This is the recommended hook to use when a component intends to write to
+ * state without reading it. Allows a component to set the value without subscribing the
+ * component to re-render when the value changes.
+ *
+ * @returns A setter function for updating the value of atom state.
+ *
+ * @example
+ *
+ *   const setCounter = useSetAtomState(counterAtom);
+ *
+ * @nosideeffects
+ */
 export const useSetAtomState = /*#__INLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   return useHandler(atom.set);
 };
 
-/** @nosideeffects */
+/**
+ * @description This is the recommended hook to use when a component intends to read and
+ * write state. Using this hook in a React component will subscribe the component
+ * to re-render when the state is updated.
+ *
+ * @returns A tuple where the first element is the value of state and the second element
+ * is a setter function that will update the value of the given state when called.
+ *
+ * @example
+ *
+ *   const [counter, setCounter] = useAtomState(counterAtom);
+ *
+ * @nosideeffects
+ */
 export const useAtomState = /*#__INLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   const state = useAtomValue(atom);
   const update = useSetAtomState(atom);
@@ -48,17 +101,52 @@ export const useAtomState = /*#__INLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   return [state, update] as const;
 };
 
-/** @nosideeffects */
+/**
+ * @description Using this hook allows a component to reset the state to its default value without
+ * subscribing the component to re-render whenever the state changes.
+ *
+ * @returns A function that will reset the value of the given state to its default value.
+ *
+ * @example
+ *
+ *   const resetCounter = useResetAtomState(counterAtom);
+ *
+ * @nosideeffects
+ */
 export const useResetAtomState = /*#__INLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   return useHandler(() => atom.set(atom.default));
 };
 
-/** @nosideeffects */
+/**
+ * @description This is the recommended hook to use when a component intends to read computed state
+ * only on first render. Using this hook in a React component will **NOT** subscribe the component
+ * to re-render when the state is updated.
+ *
+ * @returns The value of the given atom state.
+ *
+ * @example
+ *
+ *   const counter = useAtomConst(counterAtom);
+ *
+ * @nosideeffects
+ */
 export const useAtomConst = /*#__INLINE__*/<T>(atom: Readonly<Atom<T>>) => {
   return useCreation(atom.get);
 };
 
-/** @nosideeffects */
+/**
+ * @description This is the recommended hook to use when a component intends to read state
+ * only on first render. Using this hook in a React component will **NOT** subscribe the component
+ * to re-render when the state is updated.
+ *
+ * @returns The computed value of the given atom state.
+ *
+ * @example
+ *
+ *   const plus = useAtomSelectorConst(counterAtom, (value) => value + 1);
+ *
+ * @nosideeffects
+ */
 export const useAtomSelectorConst = /*#__INLINE__*/<T, S>(atom: Readonly<Atom<T>>, selector: Selector<T, S>) => {
   return useCreation(() => selector(atom.get()));
 };
--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,75 @@ import type { Atom, AtomValOrUpdater, Store } from './types.js';
 import { batch, isFunction, weakUniqueId } from '@mntm/shared';
 import { default as mitt } from 'mitt';
 
+/**
+ * Special EventEmitter for state updates.
+ *
+ * @description Precoil is built around an idiomatic asynchronous event-driven
+ * architecture in which setters emits atoms keys named events that cause
+ * listeners to be called with a new state.
+ *
+ * @example
+ *
+ *   // Logs state on update atom.
+ *   updater.on(counterAtom.key, console.log);
+ *
+ * @example
+ *
+ *   // Logs state on update atom with `counter` key.
+ *   updater.on('counter', console.log);
+ *
+ * @see https://github.com/developit/mitt
+ */
 export const updater = mitt<Record<string, any>>();
 
+/**
+ * The source of truth for all created atoms.
+ *
+ * @description It is better to use the atom's method to get the current state.
+ *
+ * @example
+ *
+ *   console.log(store.get('counter')); // log: 0
+ *
+ */
 export const store: Store = new Map();
 
+/**
+ * Utility function for creating getters.
+ *
+ * @template T Atom state type.
+ *
+ * @param key Atom's key.
+ *
+ * @returns A new getter for the current state of the atom.
+ *
+ * @example
+ *
+ *   const getCounter = getter(counterAtom.key);
+ *
+ *   console.log(getCounter()); // log: 0
+ *
+ * @noinline
+ */
 export const getter = <T>(key: string) => () => store.get(key) as T;
 
+/**
+ * Utility function for creating setters.
+ *
+ * @template T Atom state type.
+ *
+ * @param key Atom's key.
+ *
+ * @returns A new setter for the current state of the atom.
+ *
+ * @example
+ *
+ *   const setCounter = setter(counterAtom.key);
+ *
+ *   console.log(setCounter(2)); // log: 2
+ *
+ * @noinline
+ */
 export const setter = <T>(key: string) => (value: AtomValOrUpdater<T>): T => {
   const current = store.get(key) as T;
   const next = isFunction(value) ? value(current) : value;
@@ -25,6 +88,28 @@ export const setter = <T>(key: string) => (value: AtomValOrUpdater<T>): T => {
   return next;
 };
 
+/**
+ * An atom represents state in precoil.
+ *
+ * @description Atoms contain the source of truth for our application state.
+ *
+ * @template T Atom state type.
+ *
+ * @param defaultValue Initial atom state.
+ *
+ * @param key A unique string that allows you to indetify the atom.
+ *
+ * @returns A new atom that lets you read and update its state.
+ *
+ * @example
+ *
+ *   const counterAtom = atom(0, 'counter');
+ *
+ *   console.log(counterAtom.get()); // log: 0
+ *   console.log(counterAtom.set(1)); // log: 1
+ *
+ * @noinline
+ */
 export const atom = <T>(defaultValue: T, key = weakUniqueId()): Atom<T> => {
   store.set(key, defaultValue);
 
