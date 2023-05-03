130c448b6db5071d1512da711bbd31ed0f376e66
eolme <im@petrov.engineer>
2023-05-03 18:27:43 +0300

    Fix webpack export analysis

--- src/batch.ts
+++ src/batch.ts
@@ -1,3 +1,3 @@
 import * as ReactDOM from 'react-dom';
 
-export const batch = ReactDOM.unstable_batchedUpdates || ((callback: () => void) => callback());
+export const batch = (ReactDOM as any)['unstable_batchedUpdates'.toString()] || ((callback: () => void) => callback());
